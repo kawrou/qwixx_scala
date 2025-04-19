1777fae1823a20141a64e1ca2a3ee937d52bbb4a
kawrou <121672581+kawrou@users.noreply.github.com>
2025-04-20 00:28:26 +0100

    feat:Player factory method for creating new Player and InvalidName error type

--- src/main/scala/game/model/Player.scala
+++ src/main/scala/game/model/Player.scala
@@ -1,13 +1,22 @@
 package game.model
 
+sealed trait PlayerCreationError extends Product with Serializable {
+  def message: String = PlayerCreationError.messageFor(this)
+}
+
+case object InvalidName extends PlayerCreationError
+
+private object PlayerCreationError {
+  def messageFor(error: PlayerCreationError): String = error match {
+    case InvalidName => "Player name must be a non-empty string."
+  }
+}
+
 case class Player(id: Int, name: String, gameCard: GameCard, penalty: Int)
 
 object Player {
-  def markNumber(player: Player, rowColor: RowColor, number: Int)= {
-    val maybeCard = player.gameCard.markNumber(rowColor, number)
-    maybeCard match {
-      case Left(err) => ???
-      case Right(card) => Player(player.id, player.name, card, player.penalty)
-    }
+  def newPlayer(id: Int, name: String): Either[InvalidName.type, Player] = {
+    if (name.trim.isEmpty) Left(InvalidName)
+    else Right(Player(id, name, GameCard.empty, 0))
   }
 }
