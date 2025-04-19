13341d8b0b35e70eb3be5f774eb61bb79e9aeff1
kawrou <121672581+kawrou@users.noreply.github.com>
2025-04-19 23:10:56 +0100

    feat:Player.markNumber updates the game card and returns a new Player

--- src/test/scala/game/model/PlayerSpec.scala
+++ src/test/scala/game/model/PlayerSpec.scala
@@ -14,5 +14,14 @@ class PlayerSpec extends AnyFunSpec  with Matchers with TableDrivenPropertyCheck
       player.gameCard.rows.keySet should contain allOf(RowColor.Red, RowColor.Yellow, RowColor.Green, RowColor.Blue)
       player.gameCard.rows.values.forall(_.isEmpty) shouldBe true
     }
+
+    it("should return an updated player when marking a number in the game card") {
+      val player = Player(1, "Test player", GameCard.empty, 0)
+      val updatedPlayer = Player.markNumber(player, RowColor.Red, 3)
+      updatedPlayer.name shouldBe "Test player"
+      updatedPlayer.id shouldBe 1
+      updatedPlayer.penalty shouldBe 0
+      updatedPlayer.gameCard.rows(RowColor.Red) should contain only 3
+    }
   }
 }
