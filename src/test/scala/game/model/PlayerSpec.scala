112b98f5ecc395fd9200def8fcbcbdb877deea00
kawrou <121672581+kawrou@users.noreply.github.com>
2025-04-19 22:45:50 +0100

    test:Write test to assert against a new player's fields

---
+++ src/test/scala/game/model/PlayerSpec.scala
@@ -0,0 +1,18 @@
+package game.model
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+class PlayerSpec extends AnyFunSpec  with Matchers with TableDrivenPropertyChecks {
+  describe("Player") {
+    it("should return a player with a name, id, GameCard, and penalty") {
+      val player = Player
+      player.id shouldBe 1
+      player.name shouldBe "Test player"
+      player.penalty should be 0
+      player.gameCard.rows.keySet should contain allOf(RowColor.Red, RowColor.Yellow, RowColor.Green, RowColor.Blue)
+      player.gameCard.rows.values.forall(_.isEmpty) shouldBe true
+    }
+  }
+}
