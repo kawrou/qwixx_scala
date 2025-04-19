da6497ec127570ab32714140e962260658b1237d
kawrou <121672581+kawrou@users.noreply.github.com>
2025-04-19 23:10:56 +0100

    feat:Player.markNumber updates the game card and returns a new Player

--- src/main/scala/game/model/Player.scala
+++ src/main/scala/game/model/Player.scala
@@ -1,3 +1,13 @@
 package game.model
 
 case class Player(id: Int, name: String, gameCard: GameCard, penalty: Int)
+
+object Player {
+  def markNumber(player: Player, rowColor: RowColor, number: Int)= {
+    val maybeCard = player.gameCard.markNumber(rowColor, number)
+    maybeCard match {
+      case Left(err) => ???
+      case Right(card) => Player(player.id, player.name, card, player.penalty)
+    }
+  }
+}
