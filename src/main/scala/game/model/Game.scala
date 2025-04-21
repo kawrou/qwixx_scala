4ac908b07701f81dda5e2484cd2caab7baabae0c
kawrou <121672581+kawrou@users.noreply.github.com>
2025-04-22 00:23:54 +0100

    feat:GameCreationError ADT

--- src/main/scala/game/model/Game.scala
+++ src/main/scala/game/model/Game.scala
@@ -2,12 +2,30 @@ package game.model
 
 import scala.util.Random
 
+sealed trait GameCreationError extends Product with Serializable {
+  def message: String = GameCreationError.messageFor(this)
+}
+
+private object GameCreationError {
+  case object NotEnoughPlayers extends GameCreationError
+
+  case object TooManyPlayers extends GameCreationError
+
+  case class PlayerError(err: PlayerCreationError) extends GameCreationError
+
+  def messageFor(error: GameCreationError): String = error match {
+    case NotEnoughPlayers => "There should be a minimum of 2 players"
+    case TooManyPlayers => "There should be a maximum of 5 players"
+    case PlayerError(err) => s"An error during creating a player ${err.message}"
+  }
+}
+
 case class Game(players: List[Player], activePlayerIndex: Int, dice: Dice, isGameOver: Boolean)
 
 object Game {
-  def startWithNames(names: List[String]): Either[String, Game] = {
-    if (names.length < 2) return Left("PlayerQuantityError: There should be a minimum of 2 players")
-    else if (names.length > 5) return Left("PlayerQuantityError: There should be a maximum of 5 players")
+  def startWithNames(names: List[String]): Either[GameCreationError, Game] = {
+    if (names.length < 2) return Left(GameCreationError.NotEnoughPlayers)
+    else if (names.length > 5) return Left(GameCreationError.TooManyPlayers)
 
     val players = names.zipWithIndex.map {
       case (name, i) => Player(id = i, name)
@@ -15,7 +33,7 @@ object Game {
 
     val (lefts, rights) = players.partitionMap(identity)
     if (lefts.nonEmpty) {
-      return Left(lefts.head.message)
+      return Left(GameCreationError.PlayerError(lefts.head))
     }
     Right(Game(
       rights,
